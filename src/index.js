980c42c16922107135725d739dec5e877b599bde
Arya Sankar Bhattacharyya <aryasbhattacharyya@gmail.com>
2024-06-27 11:30:25 +0530

    Tested user schema

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serverConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
+const User = require('./schema/userSchema');
 // const bodyParser = require('body-parser');
 
 const app = express();
@@ -12,4 +13,15 @@ app.use(express.urlencoded({extended: true}));
 app.listen(serverConfig.PORT, async() => { 
     await connectDB();
     console.log(`Server started at port ${serverConfig.PORT}...!`);
+
+    const newuser = await User.create({
+        email: 'a@b.com',
+        password: '123456',
+        firstName: 'Jhonathan',
+        lastName: 'Mathias',
+        mobileNumber: '1234567890'
+    });
+
+    console.log("Created new user");
+    console.log(newuser);
 })
