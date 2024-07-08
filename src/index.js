3a12c0ab214445f7682da44500c61d62e824e971
Arya Sankar Bhattacharyya <aryasbhattacharyya@gmail.com>
2024-07-08 15:40:59 +0530

    Cleaned up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const serverConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
-const User = require('./schema/userSchema');
 const userRouter = require('./routes/userRoute');
 const cartRouter = require('./routes/cartRoute');
 const authRouter = require('./routes/authRoute');
 const cookieParser = require('cookie-parser');
-const { isLoggedIn } = require('./validation/authValidator');
-const uploader = require('./middlewares/multerMiddleware');
-const cloudinary = require('./config/cloudinaryConfig');
-const fs = require('fs/promises');
 const productRouter = require('./routes/productRoute');
 const orderRouter = require('./routes/orderRoutes');
 
-// Const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -22,19 +16,17 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended: true}));
 
-// Routing middleware
-// If your req route starts with /user then it handles that
 app.use('/users' , userRouter); //Connects the router to the server
 app.use('/carts', cartRouter);
 app.use('/auth', authRouter);
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
 
-app.get('/ping', isLoggedIn, (req,res) => {
+app.get('/ping', (req,res) => {
     console.log(req.body);
     console.log(req.cookies);
     return res.json({
-        message: "ping"
+        message: "pong"
     });
 })
 
