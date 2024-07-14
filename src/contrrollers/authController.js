deee5a82f0956aaa7dbb660f369bce41d6a0fae3
Arya Sankar Bhattacharyya <aryasbhattacharyya@gmail.com>
2024-07-14 21:58:49 +0530

    Added some changes

--- src/contrrollers/authController.js
+++ src/contrrollers/authController.js
@@ -11,7 +11,7 @@ async function login(req,res) {
         res.cookie("authToken",response.token, {
             httpOnly: true,
             secure: COOKIE_SECURE,
-            sameSite: "None",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             domain: FRONTEND_URL
         })
@@ -40,7 +40,7 @@ async function logout(req,res){
     res.cookie("authToken", "", {
         httpOnly: true,
         secure: COOKIE_SECURE,
-        sameSite: "None",
+        sameSite: "none",
         domain: FRONTEND_URL,
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
